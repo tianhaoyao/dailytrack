06c5bd885643bd704bd38d6b434ef30a23aa77e3
ricecustomer <tianhao.yao@mail.utoronto.ca>
2019-08-31 04:02:16 -0400

    all symbols work

--- gui/arcane.js
+++ gui/arcane.js
@@ -1,8 +1,39 @@
-let vj = new Arcane(1,0,1,"Vanishing Journey");
-console.log(vj.name)
+let vj = new Arcane(20,0,14,"VJ");
+let cc = new Arcane(20,0,15,"Chu Chu");
+let lc = new Arcane(16,0,9,"Lachelein");
+let ac = new Arcane(18,0,10,"Arcana");
+let mr = new Arcane(14,0,8,"Morass");
+let es = new Arcane(12,0,8,"Esfera");
+
 document.getElementById("vjname").innerHTML = vj.name;
 document.getElementById("vjlvl").innerHTML = vj.level;
 document.getElementById("vjxp").innerHTML = vj.exp;
+
+document.getElementById("ccname").innerHTML = cc.name;
+document.getElementById("cclvl").innerHTML = cc.level;
+document.getElementById("ccxp").innerHTML = cc.exp;
+
+document.getElementById("lcname").innerHTML = lc.name;
+document.getElementById("lclvl").innerHTML = lc.level;
+document.getElementById("lcxp").innerHTML = lc.exp;
+
+document.getElementById("acname").innerHTML = ac.name;
+document.getElementById("aclvl").innerHTML = ac.level;
+document.getElementById("acxp").innerHTML = ac.exp;
+
+document.getElementById("mrname").innerHTML = mr.name;
+document.getElementById("mrlvl").innerHTML = mr.level;
+document.getElementById("mrxp").innerHTML = mr.exp;
+
+document.getElementById("esname").innerHTML = es.name;
+document.getElementById("eslvl").innerHTML = es.level;
+document.getElementById("esxp").innerHTML = es.exp;
+
+var arcane = (vj.level + cc.level + lc.level + ac.level + mr.level + es.level + 12) * 10;
+console.log(vj.level + cc.level)
+console.log(arcane)
+document.getElementById("arc").innerHTML = arcane;
+document.getElementById("stat").innerHTML = "+" + (arcane * 10);
 
 var MAX = 2679;
 
@@ -12,14 +43,6 @@ function Arcane(level,exp,growth,name) {
 	this.growth = growth;
 	this.name = name;
 
-	this.addExp= function() {
-		return ++this.exp;
-	};
-
-	this.getName = function() {
-		document.getElementById("vjname").innerHTML = this.name;
-		return this.name;
-	};
 
 	this.getTotalExp = function() {
 		var xp = 0;
@@ -77,8 +100,57 @@ function Arcane(level,exp,growth,name) {
 	}
 }
 
+function getArcane() {
+	var arcane = (vj.level + cc.level + lc.level + ac.level + mr.level + es.level + 12) * 10;
+	document.getElementById("arc").innerHTML = arcane;
+	document.getElementById("stat").innerHTML = "+" + (arcane * 10);
+}
+
 function update() {
+	updatevj();
+	updatecc();
+	updatelc();
+	updateac();
+	updatemr();
+	updatees();
+	getArcane();
+}
+
+
+function updatevj() {
 	document.getElementById("vjname").innerHTML = vj.name;
 	document.getElementById("vjlvl").innerHTML = vj.level;
 	document.getElementById("vjxp").innerHTML = vj.exp;
+}
+
+function updatecc() {
+	document.getElementById("ccname").innerHTML = cc.name;
+	document.getElementById("cclvl").innerHTML = cc.level;
+	document.getElementById("ccxp").innerHTML = cc.exp;
+}
+
+function updatelc() {
+	document.getElementById("lcname").innerHTML = lc.name;
+	document.getElementById("lclvl").innerHTML = lc.level;
+	document.getElementById("lcxp").innerHTML = lc.exp;
+}
+
+function updateac() {
+	document.getElementById("acname").innerHTML = ac.name;
+	document.getElementById("aclvl").innerHTML = ac.level;
+	document.getElementById("acxp").innerHTML = ac.exp;
+}
+
+
+function updatemr() {
+	document.getElementById("mrname").innerHTML = mr.name;
+	document.getElementById("mrlvl").innerHTML = mr.level;
+	document.getElementById("mrxp").innerHTML = mr.exp;
+}
+
+
+function updatees() {
+	document.getElementById("esname").innerHTML = es.name;
+	document.getElementById("eslvl").innerHTML = es.level;
+	document.getElementById("esxp").innerHTML = es.exp;
 }
